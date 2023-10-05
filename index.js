132e08f5472e64138af3a6a2e758db32c0ffb076
axelsagundo <axelsagundo@gmail.com>
2023-10-05 00:46:30 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    // document.querySelector('#button').addEventListener('click', () => {
+    //     console.log('You clicked me!')
+    // });
+    const toggleButton = document.getElementById('togglebutton') 
+    const navigationList = document.getElementById('navbarLinks');
+    toggleButton.addEventListener('click', () => {
+      
+        navigationList.classList.toggle("active");
+    })
+
+
+        // Select the first h1 element on the page
+    let h1Element = document.querySelector('h1');
+
+    // Get the computed style of the h1 element
+    let computedStyle = window.getComputedStyle(h1Element);
+
+    // Get the font size from the computed style
+    let fontSize = computedStyle.getPropertyValue('font-size');
+
+    console.log(fontSize); // This will print the font size to the console
+
+
+});
+
+
+
+
+
+
